7e721f944bca67c6cfdb6de9002a7d78663d4bf6
navindu-ds <navindu.20@cse.mrt.ac.lk>
2023-02-19 22:40:32 +0530

    adding-main-webapp-page

---
+++ webapp.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.markdown("# T20 Cricket Score Predictor 🏏")
+st.sidebar.markdown("# T20 Cricket Score Predictor 🏏")
+
+st.write("##### source code on [github](https://github.com/navindu-ds/T20-Cricket-Score-Predictor)")
